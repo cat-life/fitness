81250a228bdc9b4893ed2c54146f49d47864b0cb
yanweiqing <yanweiqing@meicai.cn>
2017-05-07 11:08:05 +0800

    add components guide

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,14 +18,35 @@ Page({
     showPop: false,
     popData: {
       keyValue: [7, 8, 9, 4, 5 ,6, 1, 2, 3, '.', 0, '<']
+    },
+    showToast: false,
+    toastData: {
+      message: ''
     }
   },
-  //事件处理函数
+  // 事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../home/index'
     })
   },
+  openToast: function(msg) {
+    this.setData({
+      showToast: true,
+      toastData: {
+        message: '数据提交成功'
+      }
+    });
+    setTimeout(() => {
+      this.setData({
+        showToast: false,
+        toastData: {
+          message: ''
+        }
+      });
+    }, 1000);
+  },
+  // 打开pop
   openPop: function() {
     this.setData({
       showPop: true,
@@ -39,6 +60,7 @@ Page({
     let data = this.data.baseData;
     let { weight } = this.data.tmpData;
     console.log(weight);
+    this.openToast('成功');
     data.weight = weight.split('.')[0];
     data.weight_point = weight.split('.')[1] ? weight.split('.')[1] : '.0';
     this.setData({
@@ -56,20 +78,25 @@ Page({
     let { weight } = this.data.tmpData;
     let { value } = event.target.dataset;
 
+    // 点击删除符号
     if (value === '<') {
       if (weight !== '') {
         weight = weight.substr(0, weight.length - 1);
       }
+    // 点击小数点
     } else if (value === '.') {
       if (weight.indexOf('.') < 0) {
         weight += '.';
       }
+    // 点击数字
     } else {
+      // 有小数点的情况
       if (weight.indexOf('.') > -1) {
         if (weight.indexOf('.') >= weight.length - 1) {
           weight += value;
         }
-      } else if(weight - 0 < 100) {
+      // 无小数点的情况
+      } else if (weight - 0 < 100) {
         weight += value;
       }
     }
