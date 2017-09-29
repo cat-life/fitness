a71cdc00ebbedccb3593a698f15961962303e0d2
yanweiqing <yanweiqing@meicai.cn>
2017-09-29 10:19:19 +0800

    发现页面初始化

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -5,7 +5,10 @@ var Base = require('../../utils/base.js');
 
 Page(Object.assign({}, Base, {
   data: Object.assign({}, Base.dataSet, {
-
+    pullList: [{
+      name: '测试',
+      data: [1,2,3,4,5,6,7]
+    }]
   }),
   onLoad: function () {
   }
