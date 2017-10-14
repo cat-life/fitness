df972ca5b72e3a13607a3aa420759cd5ffa2a7c7
yanweiqing <yanweiqing@meicai.cn>
2017-10-14 10:42:59 +0800

    优化发现代码

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -2,14 +2,28 @@
 //获取应用实例
 const app = getApp()
 var Base = require('../../utils/base.js');
+var fakeList = require('./fake.js');
 
 Page(Object.assign({}, Base, {
   data: Object.assign({}, Base.dataSet, {
-    pullList: [{
+    activeTab: 'discover',
+    tabInfo: [{
+      name: '关注', key: 'follow'
+    }, {
+      name: '精选', key: 'discover'
+    }, {
+      name: '专题', key: 'topic'
+    }],
+    discoverList: [{
       name: '测试',
-      data: [1,2,3,4,5,6,7]
+      data: fakeList.discover
     }]
   }),
   onLoad: function () {
+  },
+  handleTabSwitch: function(event) {
+    this.setData({
+      activeTab: event.target.dataset.key
+    });
   }
 }));
